0e8e6b6024e49d6c4325128378dc3031881a8611
rajaNayak123 <nayakraja151@gmail.com>
2025-06-19 13:27:52 +0530

    add rule unused error variable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,7 @@ const eslintConfig = [
           "caughtErrorsIgnorePattern": "^_"
         }
       ],
+      "@typescript-eslint/no-unused-vars": "off",
       // Allow unescaped apostrophes in JSX
       "react/no-unescaped-entities": "off"
     }
